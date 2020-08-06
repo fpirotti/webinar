317ced39808e2d48226f2ca4bf269c004f534a9a
fpirotti <fpirotti@gmail.com>
2020-08-06 15:03:58 +0200

    Finished webinar 1

--- copyright.R
+++ copyright.R
@@ -1,12 +1,8 @@
-copyright<- '<div class="column cc-footer-license">
- 
-<div xmlns:cc="https://creativecommons.org/ns#" about="https://creativecommons.org">
+copyright<- '
 <p>Except where otherwise <a class="subfoot" href="https://creativecommons.org/policies#license">noted</a>, content on this site is licensed under a <a class="subfoot" href="https://creativecommons.org/licenses/by-sa/4.0/" rel="license">Creative Commons Attribution ShareAlike 4.0 International license</a>.</p>
-</div> 
 <a rel="license" target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/" title="Creative Commons Attribution ShareAlike 4.0 International license">
 <img alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Cc-by-sa_%281%29.svg/120px-Cc-by-sa_%281%29.svg.png" decoding="async" width="120" height="42" srcset="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Cc-by-sa_%281%29.svg/180px-Cc-by-sa_%281%29.svg.png 1.5x, https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Cc-by-sa_%281%29.svg/240px-Cc-by-sa_%281%29.svg.png 2x" data-file-width="120" data-file-height="42">
 </a> 
-</div> 
 <hr>
 Code available in <a target="_blank" href="https://github.com/fpirotti/webinar_series_R_GEE">GITHUB here</a> 
 '
